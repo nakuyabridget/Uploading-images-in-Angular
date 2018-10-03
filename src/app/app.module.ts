3111874101f15a7aabd6a3f7a4d302ef7a6118f0
nakuyabridget <bnakuya7@gmail.com>
2018-10-03 11:31:39 +0300

    ng g c upload image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImageComponent
   ],
   imports: [
     BrowserModule
